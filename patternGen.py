cc5f1d23a0c04f7ccac04eb54ecaf2ac67990d58
kcl <2097088648@qq.com>
2018-05-10 19:41:49 +0800

    5/10 add bitstreamGen

--- patternGen.py
+++ patternGen.py
@@ -1,10 +1,13 @@
 import re, sys, os, struct
 import bs4
 from bs4 import BeautifulSoup
+import time
 import timeit
 
-
-DIRECTORY = os.path.join(sys.path[0], "pin_test")
+DIRECTORY = sys.path[0]
+# TB_RELPATH = os.path.join(sys.path[0], "pin_test")
+TB_RELPATH = "pin_test"
+BS_RELPATH = "pin_test_bitstream"
 signal = {}
 
 # define operation code here
@@ -18,6 +21,18 @@ BIN_WIDTH = 128
 BIN_BYTE_WIDTH = BIN_WIDTH / 8
 # enddefine
 
+"""Tools"""
+
+
+# decorator without parameter
+def timer(func):
+	def _timer():
+		start_time = time.time()
+		func()
+		end_time = time.time()
+		print("\nTotal time: " + str(end_time-start_time))
+	return _timer
+
 
 def name_check(file, name):
 	filename = os.path.splitext(file)[0]
@@ -27,8 +42,8 @@ def name_check(file, name):
 		print(file + " name mismatch!")
 
 
-def get_soup(file):
-	path = os.path.join(DIRECTORY, file)
+def get_soup(relpath, file):
+	path = os.path.join(DIRECTORY, relpath, file)
 	with open(path, "r") as f:
 		soup = BeautifulSoup(f.read(), "xml")
 	return soup
@@ -96,8 +111,8 @@ def write_mask(fw, sig_dict, pio_dict, tb_counter):
 """File Parsers"""
 
 
-def tcf_parser(file, sig2pin):
-	soup = get_soup(file)
+def tcf_parser(relpath, file, sig2pin):
+	soup = get_soup(relpath, file)
 	name_check(file, soup.TCF['name'])
 	for key, value in sig2pin.items():
 		# FRAME & ASSEMBLY tag
@@ -130,9 +145,14 @@ def tcf_parser(file, sig2pin):
 		sig2pin[key] = (byte, bit)
 """
 
+def itm_parser(relpath, file):
+	soup = get_soup(relpath, file)
+	name_check(file, soup.ITEM['name'])
+	return soup.find('CYCLE')['period']
 
-def lbf_parser(file, sig2pin):
-	soup = get_soup(file)
+
+def lbf_parser(relpath, file, sig2pin):
+	soup = get_soup(relpath, file)
 	name_check(file, soup.LBF['name'])
 	# dut_tag = soup.find('DUT').children
 	for key in sig2pin:
@@ -141,16 +161,29 @@ def lbf_parser(file, sig2pin):
 	return sig2pin
 
 
-def itm_parser(file):
-	soup = get_soup(file)
-	name_check(file, soup.ITEM['name'])
-	return soup.find('CYCLE')['period']
+def tfo_parser(relpath, file):
+	"""
+	TODO: Multiple TEST tag, different attr 'name' and 'path'.
+	TODO: Return file_dict like {test1:{path:'path', file1:'file1', ...}, test2:{...}, ...}.
+	Format: {"lbf":"LB010tf1", dut:"LX200", "test":{"pin_test":{"path":".", }}}
+	:param file:
+	:return file_dict:
+	"""
+	soup = get_soup(relpath, file)
+	name_check(file, soup.TFO['name'])
+	file_dict = {}
+	test_tag = soup.find('TEST')
+	file_dict['relpath'] = test_tag['path']
+	for child in test_tag.children:
+		if type(child) == bs4.element.Tag:
+			file_dict[child.name] = child['name'] + '.' + child.name.lower()
+	return file_dict
 
 
-def pio_parser(file):
+def pio_parser(relpath, file):
 	pio_dict = {}
 	entri_dict = {}
-	path = os.path.join(DIRECTORY, file)
+	path = os.path.join(DIRECTORY, relpath, file)
 	regex = re.compile(r'NET\s+"(.+)"\s+DIR\s*=\s*(input|output|inout)(.*);', re.I)
 	regex2 = re.compile(r'.*"(.*)"\s*')
 	with open(path, "r") as f:
@@ -168,9 +201,9 @@ def pio_parser(file):
 	return pio_dict, entri_dict
 
 
-def ucf_parser(file):
+def ucf_parser(relpath, file):
 	sig2pin = {}
-	path = os.path.join(DIRECTORY, file)
+	path = os.path.join(DIRECTORY, relpath, file)
 	regex = re.compile('NET "(.+)" LOC = (.+);', re.I)
 	with open(path, "r") as f:
 		for line in f.readlines():
@@ -180,21 +213,21 @@ def ucf_parser(file):
 	return sig2pin
 
 
-def vcd_parser(file, sig_dict, pio_dict, entri_dict={}):
+def vcd_parser(relpath, file, sig_dict, pio_dict, entri_dict={}):
 	tick = -1         # current tick
 	tb_counter = -1    # length of testbench in operation code
 	def_state = True  # definition state
 	sym_dict = {}     # {symbolic_in_vcd: signal_name}
 	pos_dict = {}     # {position(bit): signal(1|0|z|x)}
-	path = os.path.join(DIRECTORY, file)
-	write_path = os.path.join(DIRECTORY, os.path.splitext(file)[0] + ".bin")
+	path = os.path.join(DIRECTORY, relpath, file)
+	write_path = os.path.join(DIRECTORY, relpath, os.path.splitext(file)[0] + ".bin")
 	regex1 = re.compile(r'\$var .+ \d+ (.) (.+) \$end', re.I)   # match signal name
 	regex2 = re.compile(r'#(\d+)')                              # match period
 	regex3 = re.compile(r'([0|1|x|z])(.)')                      # match testbench
 
 	if not os.path.exists(write_path):
 		os.mknod(write_path)
-	with open(path, "r") as f, open(write_path, "r+b") as fw:
+	with open(path, "r") as f, open(write_path, "w+b") as fw:
 		for line in f.readlines():
 			# end of file
 			if line == '$dumpoff':
@@ -248,29 +281,117 @@ def vcd_parser(file, sig_dict, pio_dict, entri_dict={}):
 		write_tb_op(fw, tb_counter)
 
 
-def tfo_parser(file):
+"""Bitstream related"""
+
+
+def get_sig_value(value=0, default=0, flag='const', tick=0):
 	"""
-	TODO: Multiple TEST tag, different attr 'name' and 'path'.
-	TODO: Return file_dict like {test1:{path:'path', file1:'file1', ...}, test2:{...}, ...}.
-	Format: {"lbf":"LB010tf1", dut:"LX200", "test":{"pin_test":{"path":".", }}}
-	:param file:
-	:return file_dict:
+	format:
+		flag = 'const': value = 0/1, default = (don't care)
+		flag = 'square': value = (don't care), default = 0/1
+		flag = 'T': value=[[number1, 0/1], [number2, 0/1], ...], default = 0/1
+	:param value:
+	:param default:
+	:param flag:
+	:param tick:
+	:return:
 	"""
-	soup = get_soup(file)
-	name_check(file, soup.TFO['name'])
+	if flag == 'const':
+		return value
+	if flag == 'square':
+		return (tick + default) % 2
+	if flag == 'T':
+		if tick < value[0][0]:
+			return default
+		else:
+			for t, v in value:
+				if tick > t:
+					return v
+
+
+def atf_parser(relpath, file):
+	soup = get_soup(relpath, file)
+	name_check(file, soup.ATF['name'])
+	# print(soup)
+
 	file_dict = {}
-	test_tag = soup.find('TEST')
-	for child in test_tag.children:
+	dwm_tag = soup.ATF.LIST.DWM
+	file_dict['relpath'] = dwm_tag['path']
+	print(dwm_tag)
+	for child in dwm_tag.children:
+		print(child)
 		if type(child) == bs4.element.Tag:
 			file_dict[child.name] = child['name'] + '.' + child.name.lower()
 	return file_dict
 
 
-def prepare(file):
-	pass
+def sbc_parser(relpath, file):
+	soup = get_soup(relpath, file)
+	name_check(file, soup.SBC['name'])
+
+	# Handle SIG tag
+	SIG_dict = {}  # {name:{value, default, flag}, ...}
+	for element in soup.find_all('SIG'):
+		# print(element)
+		ele_value = element['value']
+		regex1 = re.compile(r'(const)([0|1])')  # flag = const
+		m1 = regex1.search(ele_value)  # const has the highest priority
+		if m1:
+			flag, value = m1.groups()
+		else:
+			regex2 = re.compile(r'^(square)(\d+)T$')  # flag = square
+			m2 = regex2.match(ele_value)
+			if m2:
+				flag, value = m2.groups()
+				value = int(value)
+			else:
+				regex3 = re.compile(r'(\d+)T([0|1])')  # flag = T
+				m3 = regex3.findall(ele_value)
+				if m3:
+					value = [map(int, x) for x in m3]
+					flag = 'T'
+				else:  # collect default
+					flag = ''
+					value = 0
+		default = int(element['default'])
+		SIG_dict[element['name']] = {'value': value, 'flag': flag, 'default': default}
+
+	# Handle BTC tag
+	BTC_dict = {}
+	btc_tag = soup.find('BTC')
+	# print(btc_tag)
+	BTC_dict['start'] = int(btc_tag['start'][:-1])
+	for child in btc_tag.find_all('DATA'):
+		# print(children)
+		num = (int(child['byte']) - 1) * 8 + 7 - int(child['bit'])
+		BTC_dict[num] = child['name']
+	return SIG_dict, BTC_dict
 
 
 """Main process and test"""
+
+
+def testbenchGen():
+	pio_dict, entri_dict = pio_parser(TB_RELPATH, "pin_test.pio")
+	print("pio_dict = " + str(pio_dict))
+	print("entri_dict = " + str(entri_dict))
+	signal = ucf_parser(TB_RELPATH, "pin_test.ucf")
+	print(signal)
+	lbf_parser(TB_RELPATH, "LB0101.lbf", signal)
+	print(signal)
+	tcf_parser(TB_RELPATH, "F93K.tcf", signal)
+	print("sig_dict = " + str(signal))
+	vcd_parser(TB_RELPATH, "pin_test.vcd", signal, pio_dict, entri_dict)
+	print("Finished")
+
+
+def bitstreamGen():
+	pio_dict, entri_dict = pio_parser(BS_RELPATH, "LX200.spio")
+	print("pio_dict = " + str(pio_dict))
+	signal = ucf_parser(BS_RELPATH, "LX200.sucf")
+	print(signal)
+	lbf_parser("LB0101.lbf", signal)
+	print(signal)
 
 
 def main():
@@ -283,19 +404,19 @@ def main():
 	write_mask(pio_dict)
 	vcd_parser("pin_test.vcd", signal)
 
-
+@timer
 def test():
-	pio_dict, entri_dict = pio_parser("pin_test.pio")
-	print("pio_dict = "+str(pio_dict))
-	print("entri_dict = "+str(entri_dict))
-	signal = ucf_parser("pin_test.ucf")
-	print(signal)
-	lbf_parser("LB0101.lbf", signal)
-	print(signal)
-	tcf_parser("F93K.tcf", signal)
-	print("sig_dict = "+str(signal))
-	vcd_parser("pin_test.vcd", signal, pio_dict, entri_dict)
-	print("Finished")
+	SIG_dict, BTC_dict = sbc_parser(BS_RELPATH, 'LX200.sbc')
+	print(SIG_dict)
+	print(BTC_dict)
+	sfile_dict = atf_parser(BS_RELPATH, 'pin_test.atf')
+	print(sfile_dict)
+	file_dict = tfo_parser(TB_RELPATH, 'tfo_demo.tfo')
+	print(file_dict)
+	# with open("pin_test_bitstream/pin_test.rbt", 'r') as f:
+	# 	while f.readline():
+	# 		print(f.readline())
+	# end = time.time()
 
 
 test()
